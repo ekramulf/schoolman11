87019d609813e55d354e138450a25319c766316c
ekramulf <ekramulf@users.noreply.github.com>
2024-06-20 12:15:15 +0600

    Session Table Renamed

--- database/migrations/2024_06_01_105837_create_sessions_table.php
+++ database/migrations/2024_06_01_105837_create_sessions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('sessionsyear', function (Blueprint $table) {
             $table->id();
-            $table->string('sessionyear')->unique('UK_SESSION_ID');
+            $table->string('sessionyr')->unique('UK_SESSION_ID');
             $table->char('status')->default('0');
             $table->timestamps();
         });
