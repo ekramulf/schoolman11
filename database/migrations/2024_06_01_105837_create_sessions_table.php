1c58c486252f8c0b7382eab5c65edebbe146e8ba
ekramulf <ekramulf@users.noreply.github.com>
2024-06-20 12:11:23 +0600

    Session Year Table Changed

--- database/migrations/2024_06_01_105837_create_sessions_table.php
+++ database/migrations/2024_06_01_105837_create_sessions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sessions', function (Blueprint $table) {
+        Schema::create('sessionsyear', function (Blueprint $table) {
             $table->id();
             $table->string('sessionyear')->unique('UK_SESSION_ID');
             $table->char('status')->default('0');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('sessionsyear');
     }
 };
