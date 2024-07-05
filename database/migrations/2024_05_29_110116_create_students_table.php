d33a7ff7af575c264ffe5381096afd37230052fe
ekramulf <ekramulf@users.noreply.github.com>
2024-07-05 21:57:19 +0600

    stud Tbl modified

--- database/migrations/2024_05_29_110116_create_students_table.php
+++ database/migrations/2024_05_29_110116_create_students_table.php
@@ -9,26 +9,51 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+
+    /*
+        CREATE TABLE IF NOT EXISTS `students` (
+        `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+        `deptcode` varchar(255) NOT NULL,
+        `sessionyr` varchar(255) NOT NULL,
+        `studroll` varchar(255) NOT NULL,
+        `studregno` varchar(255) NOT NULL,
+        `studname` varchar(255) NOT NULL,
+        `gender` varchar(255) NOT NULL,
+        `studphoto` varchar(255) NOT NULL DEFAULT '',
+        `idcardno` varchar(255) NOT NULL,
+        `status` varchar(255) NOT NULL DEFAULT '1',
+        `created_by` varchar(255) NOT NULL DEFAULT '',
+        `created_at` timestamp NULL DEFAULT NULL,
+        `updated_at` timestamp NULL DEFAULT NULL,
+        PRIMARY KEY (`id`),
+        UNIQUE KEY `students_studroll_unique` (`studroll`),
+        UNIQUE KEY `students_studregno_unique` (`studregno`),
+        UNIQUE KEY `students_idcardno_unique` (`idcardno`),
+        KEY `IDX_DEPT_STUDID` (`deptcode`,`idcardno`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    */
+     public function up(): void
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('sroll');
-            $table->string('sreg');
-            $table->string('sname');
+            $table->string('deptcode');
             $table->string('sessionyr');
-            $table->string('deptcd')->default('');
-            $table->date('sdob');
+            $table->string('studroll')->unique('UK_STUDROLL');
+            $table->string('studregno')->unique('UK_STUDREG');
+            $table->string('studname');
+            $table->char('gender')->default('M');
+            $table->string('studphoto');
+            $table->string('idcardno')->unique('UK_STUDIDNO');
+            $table->char('status')->default('1');
+            $table->date('dob')->default('2000-01-01');
             $table->string('fname');
             $table->string('mname');
-            $table->string('simg');
-            $table->string('cardid')->default('');
             $table->string('addr1')->default('');
             $table->string('addr2')->default('');
             $table->string('mobile1')->default('');
             $table->string('mobile2')->default('');
-            $table->string('status')->default('1');
             $table->timestamps();
+            $table->index(['deptcode', 'idcardno'], 'IDX_DEPT_STUDID');
         });
     }
 
